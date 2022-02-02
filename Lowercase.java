d5bc53d224e0e020487ec17777747066e6f36436
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-02-02 23:03:56 +0530

    Create Lowercase.java
    Write a Java program to convert a given string into lowercase.

---
+++ Lowercase.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.*;
+
+
+public class Solution1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a string");
+        String userinput = sc.nextLine();
+        userinput = userinput.toLowerCase();
+        System.out.println(userinput);
+    }
+}
