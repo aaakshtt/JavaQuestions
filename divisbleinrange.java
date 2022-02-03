960147ae21dafd2e8e49a2bd61c5799b09edc607
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-02-03 21:46:45 +0530

    Create divisbleinrange.java

---
+++ divisbleinrange.java
@@ -0,0 +1,21 @@
+
+package com.company;
+import java.util.*;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        int x =5;
+        int y =20;
+        int p =3;
+        System.out.println(result(x,y,p));
+    }
+    public static int result(int x,int y,int p ){
+        if (x%p == 0)
+            return (y/p - x/p +1);
+        else {
+            return (y / p - x / p);
+        }
+    }
+}
+
+
