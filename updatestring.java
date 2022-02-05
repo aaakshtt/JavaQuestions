a80357569d50a3707f3afbf5278160118d2cf686
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-02-05 13:58:58 +0530

    Create updatestring.java
    Write a Java program to insert a word in the middle of the another string. Go to the editor
    Insert "Tutorial" in the middle of "Python 3.0", so result will be Python Tutorial 3.0

---
+++ updatestring.java
@@ -0,0 +1,14 @@
+
+package com.company;
+import java.util.*;
+
+public class Solution1
+{
+    public static void main(String args[]) {
+        String str = "Python 3.0";
+        String word = "Tutorial";
+        System.out.println(str.substring(0,7) + word +str.substring(6));
+
+    }
+
+}
