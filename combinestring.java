06ab5f5b8abfcdf762077ede165b85cbcaf6a6eb
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-02-05 14:07:41 +0530

    Create combinestring.java
    // Write a Java program to create a string in the form short_string + long_string + short_string from two strings. The strings must not have the same length. Go to the editor
    // Test Data: Str1 = Python

---
+++ combinestring.java
@@ -0,0 +1,15 @@
+// Write a Java program to create a string in the form short_string + long_string + short_string from two strings. The strings must not have the same length. Go to the editor
+// Test Data: Str1 = Python
+package com.company;
+import java.util.*;
+
+public class Solution1
+{
+    public static void main(String args[]) {
+        String str1 = "Python";
+        String str2 = "Tutorial";
+        System.out.println(str1+str2+str1);
+
+
+    }
+}
