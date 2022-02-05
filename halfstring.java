b7c6ebaf40319ccf0d8ef24e1539ce374b5818aa
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-02-05 14:06:00 +0530

    Create halfstring.java
    Write a Java program to extract the first half of a string of even length.

---
+++ halfstring.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.*;
+
+public class Solution1
+{
+    public static void main(String args[]) {
+        String str = "Python";
+        int half =(str.length())/2;
+        System.out.println(str.substring(0,half));
+    }
+}
