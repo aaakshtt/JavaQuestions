5396418463415ab71b18d6a0da2aa770213eecea
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-02-05 14:50:14 +0530

    Create arraylastandfirst.java
    Write a Java program to test if the first and the last element of an array of integers are same. The length of the array must be greater than or equal to 2

---
+++ arraylastandfirst.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Solution1
+{
+    public static void main(String args[]) {
+        int test[] = {10,20,30,40,10};
+        System.out.println(test[0] == test[test.length-1]);    
+    }
+
+}
