46c5291c2e3d4744c603bbb3263d13981c60d005
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-02-05 14:09:48 +0530

    Create stringoperation.java
    Write a Java program to create the concatenation of the two strings except removing the first character of each string. The length of the strings must be 1 and above

---
+++ stringoperation.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.*;
+
+public class Solution1
+{
+    public static void main(String args[]) {
+        String str1 = "Python";
+        String str2 = "Tutorial";
+        String fstr1 = str1.substring(1);
+        String fstr2 = str2.substring(1);
+        System.out.println(fstr1+fstr2);
+
+
+
+    }
+}
