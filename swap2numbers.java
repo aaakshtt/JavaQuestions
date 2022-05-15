1e20bc2c5bf02ceecb984f3af36dd3db8b9bc67a
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-05-15 23:23:20 +0530

    Create swap2numbers.java

---
+++ swap2numbers.java
@@ -0,0 +1,20 @@
+package com.akshat;
+import java.util.*;
+import java.util.Arrays;
+public class problems {
+	public static void main(String args[]) 
+	{
+		//Function to swap 2 numbers
+		//Using temproary variable
+		int temp ;
+		int a , b;
+		a=5;
+		b=6;
+		temp =b;
+		b=a;
+		a=temp;
+		System.out.println("Value of a is    "+ a+"Value of b is   "+ b);
+}
+	}
+
+
