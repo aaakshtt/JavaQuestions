f27272b92e537cae57e1a5342c1b9ea6be7ea66e
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2022-06-04 19:40:46 +0530

    print rectangle star pattern

---
+++ rectangle_pattern.java
@@ -0,0 +1,21 @@
+package com.akshat;
+//Java program to demonstrate
+//working of Arrays.toString()
+
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+class problems {
+	public static void main(String[] args){
+		for(int i =1;i<=4;i++) {          //Outer loop
+			for(int j= 1;j<=5;j++) {        //Inner loop
+			System.out.print("*");
+		}
+			System.out.print("\n");
+		}
+	}
+}
+// *****
+// *****
+// *****
+// *****
