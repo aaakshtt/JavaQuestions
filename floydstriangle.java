8caafba6a79776c3539d9725fc5d4fbd9d3decb9
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2023-01-26 19:20:30 +0530

    Floyd's Trinangle in Java

---
+++ floydstriangle.java
@@ -0,0 +1,17 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int count = 5;
+        int temp =1;
+        int line = 1;
+        for(int i =1;i<=line &temp<16 ;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print(temp);
+                temp++;
+            }
+            line++;
+            System.out.println();
+            
+        }
+    }
+}
