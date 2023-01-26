7b43b66fcd4748a415c0ef9943d44bc7349b2b44
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2023-01-26 15:16:35 +0530

    Write a Java program to extract the first half of a string of even length.

---
+++ printfirsthalf.java
@@ -0,0 +1,15 @@
+public class Solution {
+	
+    public static void main(String[] args) {
+    String str= "Python";
+    System.out.println(str.length());
+    for(int i =0;i<str.length()/2;i++) {
+    	System.out.print(str.charAt(i));
+    }
+   
+    }
+    
+     
+     
+}
+  
