2374da3faa4d2f525c76e28b2889fd40abc91d2d
Akshat Hole <54096952+aaakshtt@users.noreply.github.com>
2023-01-27 15:39:33 +0530

    Find the factorial

---
+++ factorial.java
@@ -0,0 +1,11 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+       int a= 9;
+       int fact = 1;
+       for(int i =a;i>0;i--){
+           fact = fact*i;
+       }
+       System.out.println(fact);
+    }
+}
